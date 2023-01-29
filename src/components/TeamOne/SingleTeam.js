026ee5c344bcd864462a7d3b85e839198a1f5c62
gobelius <bego.alibasic+1@gmail.com>
2023-01-29 19:07:55 +0100

    pokusavam nesto

--- src/components/TeamOne/SingleTeam.js
+++ src/components/TeamOne/SingleTeam.js
@@ -5,15 +5,15 @@ const SingleTeam = ({ team = {} }) => {
   const { image, name, title, socials } = team;
 
   return (
-    <Col xl={3} lg={6} md={6} className="animated fadeInLeft">
-      <div className="team-one__single">
-        <div className="team-one__img">
-          <Image src={require(`@/images/team/${image}`).default.src} alt="" />
+    <Col xl={3} lg={6} md={6} className='animated fadeInLeft'>
+      <div className='team-one__single'>
+        <div className='team-one__img'>
+          {/* <Image src={require(`@/images/team/${image}`).default.src} alt="" /> */}
         </div>
-        <div className="team-one__content">
-          <h4 className="team-one__name">{name}</h4>
-          <p className="team-one__title">{title}</p>
-          <div className="team-one__social">
+        <div className='team-one__content'>
+          <h4 className='team-one__name'>{name}</h4>
+          <p className='team-one__title'>{title}</p>
+          <div className='team-one__social'>
             {socials.map(({ id, href, icon }) => (
               <a href={href} key={id}>
                 <i className={icon}></i>
